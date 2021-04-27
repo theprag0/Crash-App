96bb9c8a3798ee98d2b02f51582924f97985b784
Praghadiesh <praghadieshnikhil234@gmail.com>
2021-04-27 17:07:53 +0530

    add custom troll time

--- public/js/crash.js
+++ public/js/crash.js
@@ -4,15 +4,17 @@ const crash = document.querySelector('#crash'),
 
 // Decode base-64 encoded string
 let trollText = urlParams.get('troll-text');
-let trollImg = window.location.href.split('troll-img=')[1];
+let trollImg = urlParams.get('troll-img') && window.location.href.split('troll-img=')[1].split('&troll-time')[0];
+let trollTime = urlParams.get('troll-time');
+let closeTime = trollTime === null ? 3600 : parseFloat(trollTime) * 1000;
 
 setTimeout(function(){
     loader.style.display = 'none';
-    crash.innerHTML = (trollImg && trollText) === undefined ? 
+    crash.innerHTML = (trollImg && trollText) === null ? 
     '<p>You are fat.</p> <img src="./public/assets/Trollface.svg" class="meme"/>'
     : `<p>${atob(trollText)}</p> <img src="${atob(trollImg)}" class="meme"/>`
 },3000);
 
 setTimeout(function(){
     window.close();
-},3600);
+}, closeTime);
